df2df23500699564ab78a2cf320a8d6f6012a787
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-21 18:06:24 +0100

    generaz.mail random con ciclo v.for su <li> html

--- js/main.js
+++ js/main.js
@@ -10,9 +10,7 @@ la funzione createApp*/
 createApp({
     data() {
         return {
-            contacts: {
-                email: 'clod2303@gmail.com',
-            }
+            email: []
         }
     },
 
@@ -22,16 +20,12 @@ createApp({
     },
 
     beforeMount() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                console.log(response.data);
-                this.contacts.email = response.data.response
-                console.log(response.data.response)
-            });
+        for (let i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.email.push(response.data.response);
+                    console.log(this.email)
+                });
+        }
     },
 }).mount('#app')
-
-//    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-//   .then(function (response){
-//     const result = response.data;
-//    });
